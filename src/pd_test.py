d9fcb5baca6fa3d9a73a2fcec4189afb3f449e5d
Eddie <eddie.livengood@gmail.com>
2021-05-17 11:00:56 +0200

    15 sample CSV files from open data portals added and processed, need to assess accuracy

--- src/pd_test.py
+++ src/pd_test.py
@@ -1,3 +1,4 @@
+import os
 from pprint import pprint
 
 import pandas as pd
@@ -9,15 +10,27 @@ Pytheas = pytheas.API()
 #load pretrained rule weights
 Pytheas.load_weights('pytheas/trained_rules.json')
 
-filepath = '../data/Upload___Buergschaftsregister_fuer_HmbTG_31.12.2015_.csv'
-file_annotations = Pytheas.infer_annotations(filepath)
-delimiter = file_annotations['discovered_delimiter']
-pprint(file_annotations)
+directory = '../data/open_data/'
 
-cnt = 1
-for table in file_annotations["tables"]:
-    header = table['header']
-    nrows = table['data_end']
-    df = pd.read_csv(filepath, delimiter=delimiter, header=header, nrows=nrows)
-    df.to_csv(f'./results/csv{cnt}.csv')
-    cnt += 1
+for file in os.listdir(directory):
+    filepath = directory + file
+    file_name = file.split('/')[-1].split('.')[0]
+
+    print('PROCESSING:', file_name)
+
+    try:
+        file_annotations = Pytheas.infer_annotations(filepath, max_lines=500)
+        delimiter = file_annotations['discovered_delimiter']
+
+        cnt = 1
+        for table in file_annotations["tables"]:
+            header = table['header']
+            #if isinstance(header, list):
+            nrows = table['data_end']
+            df = pd.read_csv(filepath, delimiter=delimiter, header=header, nrows=nrows)
+            print(df.head())
+            df.to_csv(f'./results/{file_name}_{cnt}.csv', index=False)
+            cnt += 1
+    except Exception as e:
+        print(f"***ERROR***\nFile: {file_name}\n{e}")
+        pass
